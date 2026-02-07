be8686d8e65554baf993c51a45f40aeb6ae2d8ac
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2026-02-07 11:52:28 +0530

    Add method to find duplicates in an array
    Implement a method to find all duplicates in an array using index mapping.

---
+++ 05-Sorting/Find All Duplicates in an Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        ArrayList<Integer> list=new ArrayList<>();
+        int i=0;
+        while(i<nums.length){
+            int correct=nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                int temp=nums[i];
+                nums[i]=nums[correct];
+                nums[correct]=temp;
+            }else{
+                i++;
+            }
+        }
+        for(int index=0;index<nums.length;index++){
+            if(nums[index]!=index+1){
+                list.add(nums[index]);
+            }
+        }
+        return list;
+        
+    }
+}
