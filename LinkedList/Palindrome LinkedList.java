296eea9b9d654e7493398dc3b0027856ee5f35c1
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2026-02-22 18:00:46 +0530

    Add palindrome linked list solution
    Implement palindrome check for singly-linked list.

---
+++ LinkedList/Palindrome LinkedList.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head , slow = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode secondHalf = reverse(slow.next);
+        ListNode firstHalf = head;
+
+        while(secondHalf != null){
+            if(firstHalf.val != secondHalf.val) return false;
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+    public ListNode reverse(ListNode head){
+        ListNode temp = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = temp;
+            temp = head;
+            head = next;
+        }
+        return temp;
+    }
+}
