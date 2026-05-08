7908b39219c5417ac45b824eb611801073cb1664
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2026-05-08 11:38:53 +0530

    Implement largestRectangleArea function in Java

---
+++ Stack/Largest Rectangle in Histogram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            while(!stack.isEmpty() && heights[stack.peek()] > heights[i]){
+                int element = stack.pop();
+                int nse = i;
+                int pse = stack.isEmpty() ? -1 : stack.peek();
+
+                maxArea = Math.max(maxArea,(heights[element] * (nse - pse -1)));
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()){
+            int nse = n;
+            int element = stack.pop();
+            int pse = stack.isEmpty() ? -1 : stack.peek();
+            maxArea = Math.max(maxArea,(heights[element] * (nse - pse -1)));
+        }
+        return maxArea;
+    }
+}
