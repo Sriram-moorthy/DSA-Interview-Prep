e5c2c73892ac5ba24766be0612d1b1d2e9978e3d
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2026-05-14 22:38:14 +0530

    Create Count Number of Nice Subarrays.java

---
+++ Sliding Window/Count Number of Nice Subarrays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        return count(nums,k)-count(nums,k-1);
+    }
+    public int count(int[] nums,int k){
+        int right=0,left=0,sum=0,count=0;
+        while(right<nums.length){
+            sum=sum+nums[right]%2;
+            while(sum>k){
+                sum-=nums[left]%2;
+                left++;
+            }
+            count=count+(right-left)+1;
+            right++;
+        }
+        return count;
+
+    }
+}
