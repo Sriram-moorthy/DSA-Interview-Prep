a6cb03b33f2d18f077b0eee1065aba383134f816
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2026-05-18 00:06:20 +0530

    Create Lemonade Change.java

---
+++ Greedy/Lemonade Change.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        if(bills[0] != 5) return false;
+        
+        int five = 0;
+        int ten = 0;
+        for(int i = 0 ; i < bills.length ; i++){
+            if(bills[i] == 5){
+                five++;
+            }
+            else if(bills[i] == 10){
+                if(five > 0){
+                    five--;
+                }else{
+                    return false;
+                }
+                ten++;
+            }
+            else{
+                if(ten > 0 && five >0){
+                    ten--;
+                    five--;
+                }
+                else if(five >= 3){
+                    five -= 3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
