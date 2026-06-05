6c68cf6898c6d848e2d77ed8fd82d70cc54c6062
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2026-06-05 22:13:07 +0530

    Implement solution for largest number problem

---
+++ Greedy/Largest Number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] strNums = new String[nums.length];
+        for(int i=0 ; i<nums.length ; i++){
+            strNums[i] = String.valueOf(nums[i]); 
+        }
+
+        Arrays.sort(strNums, new Comparator<String>(){
+            public int compare(String a , String b){
+                String order1 = a + b;
+                String order2 = b + a;
+
+                return order2.compareTo(order1);
+            }
+        });
+
+        if(strNums[0].equals("0")) return "0";
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ;i < strNums.length ;i++){
+            sb.append(strNums[i]);
+        }
+        return sb.toString();
+    }
+}
