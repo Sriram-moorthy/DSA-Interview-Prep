fcc5f9bb47f7bc6bf689470f08436c099afa31cc
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2026-06-06 11:35:21 +0530

    Create Meeting Rooms.java

---
+++ Greedy/Meeting Rooms.java
@@ -0,0 +1,15 @@
+class Solution {
+    static boolean canAttend(int[][] arr) {
+            if(arr.length == 0 || arr == null) return false;
+            
+            Arrays.sort(arr, (a,b) -> Integer.compare(a[0],b[0]));
+            
+            for(int i = 1 ; i < arr.length ; i++){
+                if(arr[i][0] < arr[i-1][1]){
+                    return false;
+                }
+            }
+            return true;
+        
+    }
+}
