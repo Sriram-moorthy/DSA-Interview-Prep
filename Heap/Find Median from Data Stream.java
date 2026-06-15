f74fbe7efd7666eb261b444e77f847c21c624547
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2026-06-15 23:35:59 +0530

    Add MedianFinder class for median calculation
    Implement MedianFinder class to find median from a data stream using two heaps.

---
+++ Heap/Find Median from Data Stream.java
@@ -0,0 +1,37 @@
+class MedianFinder {
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    }
+    
+    public void addNum(int num) {
+        minHeap.add(num);
+
+        if(minHeap.size() - maxHeap.size() > 1){
+            maxHeap.add(minHeap.poll());
+        }
+        if(!maxHeap.isEmpty() && minHeap.peek() < maxHeap.peek()){
+            int temp = minHeap.poll();
+            minHeap.add(maxHeap.poll());
+            maxHeap.add(temp);
+        }
+        
+    }
+    
+    public double findMedian() {
+        if(minHeap.size() == maxHeap.size()){
+            return (minHeap.peek() + maxHeap.peek())/2.0;
+        }
+        return (double)minHeap.peek();
+        
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
