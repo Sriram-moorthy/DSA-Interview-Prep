de908374dd210c99b3c1a591de2e74972b804648
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2026-07-24 20:04:34 +0530

    Implement Partition Equal Subset Sum solution

---
+++ Dynamic Programming/DP on Subsequence/Partition Equal Subset Sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int num : nums){
+            sum += num;
+        }
+        if(sum % 2 != 0) return false;
+        int target = sum/2;
+        return solve(nums, target);
+    }
+    private boolean solve(int[] nums, int target){
+
+        int n = nums.length;
+        boolean[][] dp = new boolean[nums.length][target+1];
+        for(int i = 0 ; i < n ; i++){
+            dp[i][0] = true;
+        }
+
+        if(nums[0] <= target){
+            dp[0][nums[0]] = true;
+        }
+
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 1; j <= target ; j++){
+                boolean notTake = dp[i-1][j];
+                boolean take = false;
+                if(nums[i] <= j){
+                    take = dp[i-1][j - nums[i]];
+                }
+                dp[i][j] = take || notTake;
+            }
+            
+        }
+        return dp[n-1][target];
+    }
+}
