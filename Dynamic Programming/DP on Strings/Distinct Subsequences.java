e6aa658d8c0a781abd2c2590e0702952d4447ff8
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2026-08-05 10:46:39 +0530

    Implement Distinct Subsequences solution

---
+++ Dynamic Programming/DP on Strings/Distinct Subsequences.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        if(n < m) return 0;
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i = 0 ; i <= n ; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    // private int solve(String s, String t, int i, int j, int[][] dp){
+    //     if(j < 0) return 1;
+    //     if(i < 0) return 0;
+
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     if(s.charAt(i) == t.charAt(j)){
+    //         return dp[i][j] = (solve(s, t, i-1, j-1, dp) + solve(s,t, i-1,j, dp));
+    //     }
+    //     return dp[i][j] = solve(s,t, i-1, j,dp);
+        
+    // }
+}
